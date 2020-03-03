b3f4fcf9797cd97259499c9eca87e1db5de26586
fuyousheng <123456>
2020-03-03 10:44:40 +0800

    adt

--- src/main/java/org/springbus/tree/SameTree.java
+++ src/main/java/org/springbus/tree/SameTree.java
@@ -1,6 +1,7 @@
 package org.springbus.tree;
 
 import org.springbus.TreeNode;
+import org.springbus.TreePrintUtil;
 
 public class SameTree {
 
@@ -24,16 +25,19 @@ public class SameTree {
     }
 
     public static void main(String[] args) {
-        TreeNode p1=new TreeNode();
-        p1.val=1;
-        TreeNode p2=new TreeNode();
-        p2.val=2;
-        p1.left=p2;
-        TreeNode p3=new TreeNode();
-        p3.val=1;
-        p3.right=p1;
-       boolean o= new SameTree().isSameTree(p1, p3);
-System.out.println(o);
+        TreeNode p1 = new TreeNode();
+        p1.val = 1;
+        TreeNode p2 = new TreeNode();
+        p2.val = 2;
+        p1.left = p2;
+        TreeNode p3 = new TreeNode(1);
+        TreeNode p4 = new TreeNode(2);
+        p3.right = p4;
+        boolean o = new SameTree().isSameTree(p1, p3);
+        TreePrintUtil.pirnt(p1);
+        System.out.println("-----------------------");
+        TreePrintUtil.pirnt(p3);
+        System.out.println(o);
     }
 
 }
