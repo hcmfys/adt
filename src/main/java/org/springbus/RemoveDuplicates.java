dc43f94ed94f1e2334643d3374a30b862d54640d
hcmfys@163.com <hcmfys@163.com>
2020-03-21 00:02:11 +0800

    SkipListEntry

--- src/main/java/org/springbus/RemoveDuplicates.java
+++ src/main/java/org/springbus/RemoveDuplicates.java
@@ -1,8 +1,10 @@
 package org.springbus;
 
+import javax.print.attribute.standard.PrinterURI;
+
 public class RemoveDuplicates {
 
-    static int removeDuplicates(int a[]) {
+    static int removeDuplicates(Integer a[]) {
         int slow=0;
         int fast=1;
         int n=a.length;
@@ -18,11 +20,13 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-         int a[]={1,2,2,2,2,2,2,3,3,3,333,999};
+         Integer a[]={1,2,2,2,2,2,2,3,3,3,333,999};
         int  length = removeDuplicates (a);
         for(int i=0;i<length;i++) {
             System.out.print(a[i] +" ");
         }
+        System.out.println();
+        TreePrintUtil.pirnt(TreePrintUtil.makeTree(a));
         System.out.println("");
     }
 }
