9c01bacffb114e33f0c86748708596d2ad06b92d
fuyousheng <123456>
2020-03-21 18:01:42 +0800

    adt

---
+++ src/main/java/org/springbus/tree/MaxPathSum.java
@@ -0,0 +1,54 @@
+package org.springbus.tree;
+
+
+//给定一个非空二叉树，返回其最大路径和。
+//
+// 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+//
+// 示例 1:
+//
+// 输入: [1,2,3]
+//
+//       1
+//      / \
+//     2   3
+//
+//输出: 6
+//
+//
+// 示例 2:
+//
+// 输入: [-10,9,20,null,null,15,7]
+//
+//   -10
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//输出: 42
+// Related Topics 树 深度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import org.springbus.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class MaxPathSum {
+
+    public int maxPathSum(TreeNode root) {
+
+        return  0;
+    }
+}
