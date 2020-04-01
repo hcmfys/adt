8cd181df15847e4475b51cc2e4c92883c09afb17
hcmfys@163.com <hcmfys@163.com>
2020-04-01 23:21:59 +0800

    SkipListEntry

---
+++ src/main/java/org/springbus/CaretIsValid.java
@@ -0,0 +1,91 @@
+package org.springbus;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Stack;
+
+// 20. 有效的括号
+// 难度
+// 简单
+//
+// 1470
+//
+// 收藏
+//
+// 分享
+// 切换为英文
+// 关注
+// 反馈
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+//
+// 有效字符串需满足：
+//
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 注意空字符串可被认为是有效字符串。
+//
+// 示例 1:
+//
+// 输入: "()"
+// 输出: true
+// 示例 2:
+//
+// 输入: "()[]{}"
+// 输出: true
+// 示例 3:
+//
+// 输入: "(]"
+// 输出: false
+// 示例 4:
+//
+// 输入: "([)]"
+// 输出: false
+// 示例 5:
+//
+// 输入: "{[]}"
+// 输出: true
+public class CaretIsValid {
+
+  public boolean isValid(String s) {
+    if (s == null) {
+      return false;
+    }
+    if (s.equals("")) {
+      return false;
+    }
+
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == '{' || c == '[' || c=='(') {
+        stack.push(c);
+      } else {
+        if(stack.empty()) {
+          return  false;
+        }
+        Character p = stack.pop();
+        if(p=='{'  ) {
+          if(c!='}')  {
+            return  false;
+          }
+        } else if(p=='['  ) {
+          if(c!=']')  {
+            return  false;
+          }
+        }else if(p=='('  ) {
+          if(c!=')')  {
+            return  false;
+          }
+        }
+
+      }
+    }
+    return stack.empty();
+  }
+
+  public static void main(String[] args) {
+    String s = "([)]";
+    s="([)]";
+    s="()[]{}";
+    s="{[]}";
+    System.out.println("caret  is= " + new CaretIsValid().isValid(s));
+  }
+}
---
+++ src/main/java/org/springbus/LetterConvert.java
@@ -0,0 +1,117 @@
+package org.springbus;
+
+import org.springbus.tree.RobTree;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+import java.nio.channels.NonWritableChannelException;
+
+// 6. Z 字形变换
+// 难度
+// 中等
+//
+// 619
+//
+// 收藏
+//
+// 分享
+// 切换为英文
+// 关注
+// 反馈
+// 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+//
+// 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+//
+// L   C   I   R
+// E T O E S I I G
+// E   D   H   N
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+//
+// 请你实现这个将字符串进行指定行数变换的函数：
+//
+// string convert(string s, int numRows);
+// 示例 1:
+//
+// 输入: s = "LEETCODEISHIRING", numRows = 3
+// 输出: "LCIRETOESIIGEDHN"
+// 示例 2:
+//
+// 输入: s = "LEETCODEISHIRING", numRows = 4
+// 输出: "LDREOEIIECIHNTSG"
+// 解释:
+//
+// L     D     R
+// E   O E   I I
+// E C   I H   N
+// T     S     G
+public class LetterConvert {
+
+  public String convert(String s, int numRows) {
+
+    if (s == null || s.equals("")) {
+      return "";
+    }
+
+    if (numRows == 1 || s.length()==1) {
+      return s;
+    }
+    int size = s.length();
+    int col=size;
+    String data[][] = new String[numRows][col];
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < col; j++) {
+        data[i][j] = "X";
+      }
+    }
+    int q = 0;
+    for (int i = 0; i < col; i++) {
+      System.out.println(" i=" +i);
+
+        for (int c = 0; c < numRows; c++) {
+          if (q < s.length() ) {
+            data[c][i] = s.substring(q, q + 1);
+            q++;
+          }
+
+        }
+
+        for (int c = 1; c <=numRows-2; c++) {
+          if (q < s.length() ) {
+            data[numRows - c - 1][i + c] = s.substring(q, q + 1);
+            q++;
+          }
+
+        }
+        i += numRows - 2;
+      }
+
+
+
+    String rets = "";
+
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < col; j++) {
+        if (data[i][j].equals("X")) {
+          data[i][j] = "";
+        }
+        System.out.print(data[i][j] +"\t");
+        rets += data[i][j];
+      }
+      System.out.println("");
+    }
+    return rets;
+  }
+
+  public static void main(String[] args) {
+    String t = "LEETCODEISHIRING";
+    t="PAYPALISHIRING";
+    t="A";
+    t="ABC";
+    t="hjouvsuyoypayulyeimuotehzriicfskpggkbbipzzrzucxamludfykgruowzgiooobppleqlwphapjnadqhdcnvwdtxjbmyppphauxnspusgdhiixqmbfjxjcvudjsuyibyebmwsiqyoygyxymzevypzvjegebeocfuftsxdixtigsieehkchzdflilrjqfnxztqrsvbspkyhsenbppkqtpddbuotbbqcwivrfxjujjddntgeiqvdgaijvwcyaubwewpjvygehljxepbpiwuqzdzubdubzvafspqpqwuzifwovyddwyvvburczmgyjgfdxvtnunneslsplwuiupfxlzbknhkwppanltcfirjcddsozoyvegurfwcsfmoxeqmrjowrghwlkobmeahkgccnaehhsveymqpxhlrnunyfdzrhbasjeuygafoubutpnimuwfjqsjxvkqdorxxvrwctdsneogvbpkxlpgdirbfcriqifpgynkrrefx" ;
+
+
+    int row = 2;
+    row=503;
+    String ret = new LetterConvert().convert(t, row);
+    System.out.println(ret);
+  }
+}
