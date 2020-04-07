86272be3ce596cd26cc0455bab21c45f0e8f1b52
hcmfys@163.com <hcmfys@163.com>
2020-04-07 23:28:39 +0800

    SkipListEntry

---
+++ src/main/java/org/springbus/GenerateParenthesis.java
@@ -0,0 +1,93 @@
+package org.springbus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ * <p>例如，给出 n = 3，生成结果为：
+ *
+ * <p>[ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GenerateParenthesis {
+
+  public List<String> generateParenthesis(int n) {
+    List<String> ans = new ArrayList();
+    backtrack(ans, "", 0, 0, n);
+    return ans;
+  }
+
+  /**
+   * 只有在我们知道序列仍然保持有效时才添加 '(' or ')'，而不是像 方法一 那样每次添加。我们可以通过跟踪到目前为止放置的左括号和右括号的数目来做到这一点，
+   *
+   * <p>如果我们还剩一个位置，我们可以开始放一个左括号。 如果它不超过左括号的数量，我们可以放一个右括号。
+   *
+   * <p>JavaPython
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/generate-parentheses/solution/gua-hao-sheng-cheng-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @param ans
+   * @param cur
+   * @param open
+   * @param close
+   * @param max
+   */
+  public void backtrack(List<String> ans, String cur, int open, int close, int max) {
+    System.out.println("open=" + open + " close=" + close + "  -->" + cur.length()  +" --> "+cur);
+    if (cur.length() == max * 2) {
+      ans.add(cur);
+      return;
+    }
+
+    if (open < max) {
+
+      backtrack(ans, cur + "(", open + 1, close, max);
+    }
+    if (close < open) {
+      backtrack(ans, cur + ")", open, close + 1, max);
+    }
+  }
+
+
+    public List<String> generateParenthesis2(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        // 这里 dp 数组我们把它变成列表的样子，方便调用而已
+        List<List<String>> dp = new ArrayList<>(n);
+
+        List<String> dp0 = new ArrayList<>();
+        dp0.add("");
+        dp.add(dp0);
+
+        for (int i = 1; i <= n; i++) {
+            List<String> cur = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = dp.get(j);
+                List<String> str2 = dp.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        // 枚举右括号的位置
+                        cur.add("(" + s1 + ")" + s2);
+                    }
+                }
+            }
+            dp.add(cur);
+        }
+        return dp.get(n);
+    }
+
+
+
+  public static void main(String[] args) {
+    List<String> r = new GenerateParenthesis().generateParenthesis(10);
+    System.out.println(r);
+  }
+}
